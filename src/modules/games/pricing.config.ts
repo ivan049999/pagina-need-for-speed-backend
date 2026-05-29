3afe8ff08d3c26937392f9d2cfaeb239eca9503e
Iván González Ruiz <ivan.gonzalezruiz99@gmail.com>
2026-05-29 15:56:31 +0200

    feat(games): añadir precio dinámico para Underground 2

--- src/modules/games/pricing.config.ts
+++ src/modules/games/pricing.config.ts
@@ -39,4 +39,18 @@ export const GAME_PRICING: Record<string, GamePricingConfig> = {
       },
     },
   },
+  "need-for-speed-most-wanted-2005": {
+    slug: "need-for-speed-most-wanted-2005",
+    fallbackAmount: 9.99,
+    fallbackCurrency: "EUR",
+    sources: {
+      ea: {
+        url: "https://www.ea.com/es-es/games/need-for-speed/need-for-speed-most-wanted",
+      },
+      cex: {
+        url: "https://es.webuy.com/search?q=Need+for+Speed+Most+Wanted+2005",
+        productId: "need-for-speed-most-wanted-2005",
+      },
+    },
+  },
 };
